3b15081495c6b045db57b64732b494fc872ca366
ramillers <rmillesantana@gmail.com>
2024-03-03 18:03:12 -0300

    feat: add account creation functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 const express = require('express');
+const { v4: uuidv4 } = require("uuid")
 
 const app = express();
+
+app.use(express.json()); //middleware pra receber um json
+
+//fake database 
+const customers = [];
+
+//criação de conta
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body; 
+
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({error: "Usuário já existe! "})
+    }
+
+    //função de cadastrar nova conta 
+    customers.push({
+        cpf, 
+        name, 
+        id: uuidv4(),
+        statement: [],
+    });
+
+    return response.status(201).send(); 
+});
 
 app.listen(3333);
